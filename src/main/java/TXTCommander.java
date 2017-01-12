9d83e2ade2fd932c375d9bbb26f5a6909bfdbbf3
ScorpionOrange <scorpionorange@126.com>
2017-01-13 00:02:44 +0800

    2017.1.12：更新creatFile（）方法的内容及实现方式； 2017.1.13：增加contentToTxt（）方法，用于改写createFile（）返回的目标文件的内容；

--- src/main/java/TXTCommander.java
+++ src/main/java/TXTCommander.java
@@ -1,4 +1,6 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 
 /**
  * Created by ScorpionOrange on 2017/01/11.
@@ -8,14 +10,31 @@ public class TXTCommander {
     String filePath = "";
     String fileContent = "";
 
-    public boolean createFile(String filePath, String fileName) throws Exception{
-        boolean flag = false;
+    public File createFile(String filePath, String fileName) throws Exception{
+        //boolean flag = false;
         File file = new File(filePath + "\\" + fileName);
         try{
             if(!file.exists()){
                 file.createNewFile();
-                flag = true;
+                //flag = true;
             }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        //return flag;
+        return file;
+    }
+
+    public boolean contentToTxt(File file, String fileContent){
+        boolean flag = false;
+
+        try{
+            FileWriter fileWriter = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(fileContent);
+            bufferedWriter.close();
+            flag = true;
         }
         catch (Exception e){
             e.printStackTrace();
--- src/main/java/TXTCommanderDemo.java
+++ src/main/java/TXTCommanderDemo.java
@@ -1,15 +1,22 @@
+import java.io.File;
+
 /**
  * Created by ScorpionOrange on 2017/01/11.
  */
 public class TXTCommanderDemo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Testing");
-        /*
-        String path = "C:\\迅雷下载";
-        String name = "1.txt";
-        File file = new File(path + "\\" + name);
-        file.createNewFile();
-        */
-        System.out.println(new TXTCommander().createFile("C:\\迅雷下载", "Test.txt"));
+
+        //System.out.println(new TXTCommander().createFile("C:\\迅雷下载", "Test.txt"));
+        TXTCommander txtCommander = new TXTCommander();
+        File file = txtCommander.createFile("C:\\迅雷下载", "Test.txt");
+        boolean flag = file.exists();
+        if(flag){
+            System.out.println("创建TXT文件成功！");
+        }
+
+        flag = txtCommander.contentToTxt(file, "Now is loading!!! Let's start game!");
+        if(flag){
+            System.out.println("更新TXT文件内容成功！");
+        }
     }
 }
